cb9c9eb16b1b37416d333ecc3222e3f1eb94dd87
riccardo.fazio <riccardo.fazio@4science.it>
2019-01-07 15:16:35 +0100

    DS-4139 remove trailing slash from returnURL

--- dspace-api/src/main/java/org/dspace/authenticate/ShibAuthentication.java
+++ dspace-api/src/main/java/org/dspace/authenticate/ShibAuthentication.java
@@ -524,7 +524,7 @@ public class ShibAuthentication implements AuthenticationMethod
 			returnURL += host;
 			if (!(port == 443 || port == 80))
 				returnURL += ":" + port;
-			returnURL += "/" + contextPath + "/shibboleth-login";
+			returnURL += contextPath + "/shibboleth-login";
 
 			try {
 				shibURL += "?target="+URLEncoder.encode(returnURL, "UTF-8");
