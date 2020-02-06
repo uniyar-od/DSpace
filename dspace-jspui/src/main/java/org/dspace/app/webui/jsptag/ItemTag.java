48e6d3d1a8ba24da51abaf193642330914e7e620
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2020-02-06 16:13:04 +0100

    [IIIF-38] Add check on bundle to hide not primary bitstreams

--- dspace-jspui/src/main/java/org/dspace/app/webui/jsptag/ItemTag.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/jsptag/ItemTag.java
@@ -556,7 +556,7 @@ public class ItemTag extends TagSupport {
 
 						Bitstream[] bitstreams;
 						if (primaryBit != null
-								&& hideNotPrimaryBitstreams(context, request, pageContext, handle, primaryBit)) {
+								&& hideNotPrimaryBitstreams(context, request, pageContext, handle, bundles[i], primaryBit)) {
 							bitstreams = new Bitstream[] { primaryBit };
 						} else {
 							bitstreams = bundles[i].getBitstreams();
@@ -892,9 +892,16 @@ public class ItemTag extends TagSupport {
 	}
 
 	public static boolean hideNotPrimaryBitstreams(Context context, HttpServletRequest request, PageContext pageContext,
-			String handle, Bitstream bit) throws UnsupportedEncodingException {
+			String handle, Bundle bundle, Bitstream bit) throws UnsupportedEncodingException {
 
-		List<String> hideNotPrimary = bit.getMetadataValue(IViewer.METADATA_STRING_HIDENOTPRIMARY);
+		List<String> hideNotPrimary = bundle.getMetadataValue(IViewer.METADATA_STRING_HIDENOTPRIMARY);
+		for (String h : hideNotPrimary) {
+			if (BooleanUtils.toBoolean(h)) {
+				return true;
+			}
+		}
+
+		hideNotPrimary = bit.getMetadataValue(IViewer.METADATA_STRING_HIDENOTPRIMARY);
 		for (String h : hideNotPrimary) {
 			if (BooleanUtils.toBoolean(h)) {
 				return true;
