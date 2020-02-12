de09b6447b7047494ffa2a023c747091155872ad
Andrea Bollini <andrea.bollini@4science.it>
2020-02-12 07:41:30 +0100

    Add traverse test for resourcepolicies

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/TraverseRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/TraverseRestControllerIT.java
@@ -11,18 +11,24 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.dspace.app.rest.builder.CollectionBuilder;
 import org.dspace.app.rest.builder.CommunityBuilder;
+import org.dspace.app.rest.builder.EPersonBuilder;
 import org.dspace.app.rest.builder.ItemBuilder;
+import org.dspace.app.rest.builder.ResourcePolicyBuilder;
 import org.dspace.app.rest.matcher.CollectionMatcher;
+import org.dspace.app.rest.matcher.CommunityMatcher;
 import org.dspace.app.rest.matcher.ItemMatcher;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
+import org.dspace.authorize.ResourcePolicy;
 import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
 import org.dspace.content.Item;
 import org.dspace.core.Constants;
+import org.dspace.eperson.EPerson;
 import org.dspace.eperson.Group;
 import org.dspace.eperson.factory.EPersonServiceFactory;
 import org.hamcrest.Matchers;
@@ -127,5 +133,51 @@ public class TraverseRestControllerIT extends AbstractControllerIntegrationTest 
         getClient().perform(get("/api/core/items/" + hiddenItemInPublicCol.getID().toString() + "/owningCollection"))
             .andExpect(status().isUnauthorized());
     }
+
+    @Test
+    public void testTraverseResourcePolicy() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Community com = CommunityBuilder.createCommunity(context).withName("A reserved community").build();
+        // make the community visible only to administrators
+        authorizeService.removePoliciesActionFilter(context, com, Constants.READ);
+        // add an ADD policy for the eperson
+        ResourcePolicy addPolicy = ResourcePolicyBuilder.createResourcePolicy(context)
+                .withDspaceObject(com).withAction(Constants.ADD).withUser(eperson).build();
+        context.restoreAuthSystemState();
+
+        String epersonToken = getAuthToken(eperson.getEmail(), password);
+        // proof that we cannot access the restricted community
+        getClient(epersonToken).perform(get("/api/core/communities/" + com.getID().toString()))
+            .andExpect(status().isForbidden());
+        // but what happen if we try to traverse the resourcepolicy
+        getClient(epersonToken).perform(get("/api/authz/resourcepolicies/" + addPolicy.getID()))
+            .andExpect(status().isOk());
+        // should the subPath of a visible REST resource be forbidden?
+        getClient(epersonToken).perform(get("/api/authz/resourcepolicies/" + addPolicy.getID() + "/resource"))
+            .andExpect(status().isForbidden());
+    }
+    
+    @Test
+    public void testSevereTraverseResourcePolicy() throws Exception {
+        context.turnOffAuthorisationSystem();
+        Community com = CommunityBuilder.createCommunity(context).withName("A reserved community").build();
+        EPerson anotherUSer = EPersonBuilder.createEPerson(context).withEmail("another@example.com").withPassword(password).build();
+        // make the community visible only to administrators
+        authorizeService.removePoliciesActionFilter(context, com, Constants.READ);
+        // add an ADD policy for the eperson
+        ResourcePolicy addPolicy = ResourcePolicyBuilder.createResourcePolicy(context)
+                .withDspaceObject(com).withAction(Constants.ADD).withUser(eperson).build();
+        context.restoreAuthSystemState();
+
+        String anotherToken = getAuthToken(anotherUSer.getEmail(), password);
+        // proof that we cannot access the restricted community
+        getClient(anotherToken).perform(get("/api/core/communities/" + com.getID().toString()))
+            .andExpect(status().isForbidden());
+        // but what happen if we try to traverse the resourcepolicy that we cannot access directly?
+        getClient(anotherToken).perform(get("/api/authz/resourcepolicies/" + addPolicy.getID()))
+            .andExpect(status().isForbidden());
+        getClient(anotherToken).perform(get("/api/authz/resourcepolicies/" + addPolicy.getID() + "/resource"))
+            .andExpect(status().isForbidden());
+    }        
 
 }
