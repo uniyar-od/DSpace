8845dfc439cd09af1524a78ffe46cb6693278363
riccardo.fazio <riccardo.fazio@4science.it>
2020-04-02 17:29:48 +0200

    D4CRIS-901 Check metadata has a qualifier and use correct array length to join metadata array into string

--- dspace-api/src/main/java/org/dspace/submit/lookup/DSpaceWorkspaceItemOutputGenerator.java
+++ dspace-api/src/main/java/org/dspace/submit/lookup/DSpaceWorkspaceItemOutputGenerator.java
@@ -329,10 +329,11 @@ public class DSpaceWorkspaceItemOutputGenerator implements OutputGenerator
 	        	int schemaID = schema.getSchemaID();
 	        	MetadataField foundField = MetadataField.findByElement(context, schemaID, md[1], md[2]);
 	            if(foundField != null) {
-	            
+	           
+     		            int range = StringUtils.isNotBlank(md[2])? 3:2; 
 		            if (extraMetadataToKeep != null
 		                    && extraMetadataToKeep.contains(StringUtils.join(
-		                            Arrays.copyOfRange(md, 0, 3), ".")))
+		                            Arrays.copyOfRange(md, 0, range), ".")))
 		            {
 		                return true;
 		            }
