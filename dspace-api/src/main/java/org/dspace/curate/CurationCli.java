29abbf9a482a887e4e4edef4e18cdf5dfbc1b003
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2020-05-19 10:39:29 +0200

    [D4CRIS-941] Fix option description

--- dspace-api/src/main/java/org/dspace/curate/CurationCli.java
+++ dspace-api/src/main/java/org/dspace/curate/CurationCli.java
@@ -47,7 +47,7 @@ public class CurationCli
         options.addOption("T", "taskfile", true,
                 "file containing curation task names");
         options.addOption("i", "id", true,
-                "Id (handle) of object to perform task on, or 'all' to perform on whole repository");
+                "list of handles separated by space of objects to perform task on, or 'all' to perform on whole repository");
         options.addOption("q", "queue", true,
                  "name of task queue to process");
         options.addOption("e", "eperson", true,
@@ -128,7 +128,7 @@ public class CurationCli
         // now validate the args
         if (idNames.isEmpty() && taskQueueName == null)
         {
-            System.out.println("Id must be specified: a handle, 'all', or a task queue (-h for help)");
+            System.out.println("Id must be specified: multiple handles separated by space, 'all', or a task queue (-h for help)");
             System.exit(1);
         }
 
