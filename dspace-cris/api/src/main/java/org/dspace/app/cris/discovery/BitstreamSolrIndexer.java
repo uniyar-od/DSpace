36eef0eb492976a7d63ebd4f2e59f22d2b668aee
Damiano Fiorenza <damiano.fiorenza@4science.it>
2020-10-21 19:54:28 +0200

    [D4CRIS-1004] index ORIGINAL and DISPLAY bundles' bitstreams under ORIGINAL_mvuntokenized

--- dspace-cris/api/src/main/java/org/dspace/app/cris/discovery/BitstreamSolrIndexer.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/discovery/BitstreamSolrIndexer.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.solr.common.SolrInputDocument;
 import org.dspace.content.Bitstream;
@@ -26,6 +27,12 @@ public class BitstreamSolrIndexer implements SolrServiceIndexPlugin
     private static final Logger log = Logger
             .getLogger(BitstreamSolrIndexer.class);
 
+    private static final String MVUNT_SUFFIX = "_mvuntokenized";
+    
+    private static final String ORIGINAL_BUNDLE = "ORIGINAL";
+    
+    private static final String DISPLAY_BUNDLE = "DISPLAY";
+    
     @Override
     public void additionalIndex(Context context, DSpaceObject dso,
             SolrInputDocument document, Map<String, List<DiscoverySearchFilter>> searchFilters)
@@ -36,18 +43,31 @@ public class BitstreamSolrIndexer implements SolrServiceIndexPlugin
         try
         {
             List<Bundle> bb = item.getBundles();
+            List<String> unitedOriginalAndDisplay = new ArrayList<String>();
             for (Bundle b : bb)
             {
 
+            	String bundleName = b.getName();
                 List<String> bitstreams = new ArrayList<String>();
                 for (Bitstream bitstream : b.getBitstreams())
                 {
                     bitstreams.add(bitstream.getType() + "-"
                             + bitstream.getID());
                 }
-                document.addField(b.getName() + "_mvuntokenized", bitstreams);
+                
+                if (StringUtils.equals(bundleName, ORIGINAL_BUNDLE) || StringUtils.equals(bundleName, DISPLAY_BUNDLE)) {
+					unitedOriginalAndDisplay.addAll(bitstreams);
+					continue;
+				}
+                
+				document.addField(bundleName + MVUNT_SUFFIX, bitstreams);
 
             }
+            
+            if (!unitedOriginalAndDisplay.isEmpty()) {
+            	document.addField(ORIGINAL_BUNDLE + MVUNT_SUFFIX, unitedOriginalAndDisplay);
+			}
+            
         }
         catch (Exception e)
         {
