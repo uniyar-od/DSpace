705a3faa5c1c8f4d0c4b891413bce64eed6afb9b
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2020-12-02 09:45:09 +0100

    [D4CRIS-1033] use transfer manager to support multipart upload and to avoid reaching max number of connections on S3

--- dspace-api/src/main/java/org/dspace/storage/bitstore/S3BitStoreService.java
+++ dspace-api/src/main/java/org/dspace/storage/bitstore/S3BitStoreService.java
@@ -17,8 +17,11 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.transfer.Download;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.services.s3.transfer.model.UploadResult;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
@@ -141,8 +144,15 @@ public class S3BitStoreService extends ABitStoreService
         String key = getFullKey(bitstream.getStringColumn("internal_id"));
         try
         {
-            S3Object object = s3Service.getObject(new GetObjectRequest(bucketName, key));
-            return (object != null) ? object.getObjectContent() : null;
+            File tempFile = File.createTempFile("s3-disk-copy", "temp");
+            tempFile.deleteOnExit();
+
+            GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, key);
+            TransferManager transferManager = new TransferManager(s3Service);
+            Download download = transferManager.download(getObjectRequest, tempFile);
+            download.waitForCompletion();
+
+            return new DeleteOnCloseFileInputStream(tempFile);
         }
         catch (Exception e)
         {
@@ -176,10 +186,12 @@ public class S3BitStoreService extends ABitStoreService
             Long contentLength = Long.valueOf(scratchFile.length());
 
             PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, scratchFile);
-            PutObjectResult putObjectResult = s3Service.putObject(putObjectRequest);
+            TransferManager transferManager = new TransferManager(s3Service);
+            Upload upload = transferManager.upload(putObjectRequest);
+            UploadResult uploadResult = upload.waitForUploadResult();
 
             bitstream.setColumn("size_bytes", contentLength);
-            bitstream.setColumn("checksum", putObjectResult.getETag());
+            bitstream.setColumn("checksum", uploadResult.getETag());
             bitstream.setColumn("checksum_algorithm", CSA);
 
             scratchFile.delete();
