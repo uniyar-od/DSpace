67e7d010e7eda86925980b2a43581b9d4f4929a3
Andrea Bollini <andrea.bollini@4science.it>
2021-05-20 15:31:52 +0200

    [D4CRIS-589] develop entity creation on-the-fly by pointer

--- dspace-cris/jspui-api/src/main/java/org/dspace/app/webui/cris/controller/jdyna/CrisSearchPointerController.java
+++ dspace-cris/jspui-api/src/main/java/org/dspace/app/webui/cris/controller/jdyna/CrisSearchPointerController.java
@@ -19,10 +19,13 @@ import org.dspace.app.cris.model.ResearchObject;
 import org.dspace.app.cris.model.ResearcherPage;
 import org.dspace.app.cris.model.jdyna.widget.WidgetPointerDO;
 import org.dspace.content.DSpaceObject;
+import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
+import org.dspace.core.I18nUtil;
 
 import it.cilea.osd.jdyna.model.AWidget;
 import it.cilea.osd.jdyna.model.PropertiesDefinition;
+import it.cilea.osd.jdyna.service.IAutoCreateApplicationService;
 import it.cilea.osd.jdyna.utils.SelectableDTO;
 import it.cilea.osd.jdyna.web.controller.SearchPointerController;
 import it.cilea.osd.jdyna.web.tag.DisplayPointerTagLibrary;
@@ -31,18 +34,19 @@ import it.cilea.osd.jdyna.widget.WidgetPointer;
 public class CrisSearchPointerController extends
         SearchPointerController<PropertiesDefinition, ACrisObject>
 {
-
     private CrisSearchService searchService;
 
     @Override
     protected List<SelectableDTO> getResult(AWidget widget, String query, String expression, String... filtro)
     {
         Context context = null;
+        WidgetPointer widgetPointer = (WidgetPointer)widget;
         List<SelectableDTO> results = new ArrayList<SelectableDTO>();
         try
         {
             context = new Context();
-
+            boolean searchValueFound = false;
+            
             List<DSpaceObject> objects = getSearchService().search(context,
                     query + "*", null, true, 0, Integer.MAX_VALUE, filtro);
             for (DSpaceObject obj : objects)
@@ -53,8 +57,27 @@ public class CrisSearchPointerController extends
                 SelectableDTO dto = new SelectableDTO(
                         real.getIdentifyingValue(), display);
                 results.add(dto);
+                
+                if (display != null && display.equalsIgnoreCase(query)) {
+                	searchValueFound = true;
+                }
             }
-
+            
+            if (!searchValueFound) {
+            	String type = getPointerType(widgetPointer);
+            	if (ConfigurationManager.getBooleanProperty("cris", "widgetpointer."+type+".create-new")) {
+	            	String tag = I18nUtil
+	                        .getMessage("CrisSearchPointerController.create-new", context).trim();
+		        	Integer temporaryId = null;
+		        	if (getApplicationService() instanceof IAutoCreateApplicationService) {
+		        		temporaryId = ((IAutoCreateApplicationService)getApplicationService()).generateTemporaryPointerCandidate(type, query, tag);
+		        	}
+		        	if (temporaryId != null) {
+		        		results.add(new SelectableDTO(
+		        			Integer.toString(temporaryId), query + " " + tag));
+		        	}
+            	}
+            }
         }
         catch (Exception e)
         {
@@ -119,5 +142,26 @@ public class CrisSearchPointerController extends
         }
         return new String[] { resourcetype, filtro };
     }
-
+    
+    private String getPointerType(WidgetPointer widgetPointer) {
+    	Class target = widgetPointer.getTargetValoreClass();
+    	if (target.equals(ResearcherPage.class))
+        {
+            return CrisConstants.getEntityTypeText(CrisConstants.RP_TYPE_ID);
+        }
+        else if (target.equals(Project.class))
+        {
+        	return CrisConstants.getEntityTypeText(CrisConstants.PROJECT_TYPE_ID);
+        }
+        else if (target.equals(OrganizationUnit.class))
+        {
+        	return CrisConstants.getEntityTypeText(CrisConstants.OU_TYPE_ID);
+        }
+        else if (target.equals(ResearchObject.class))
+        {
+			return CrisConstants.getEntityTypeText(Integer.parseInt(
+					((WidgetPointerDO) widgetPointer).getFilterExtended().substring("search.resourcetype:".length())));
+        }
+    	return null;
+    }
 }
