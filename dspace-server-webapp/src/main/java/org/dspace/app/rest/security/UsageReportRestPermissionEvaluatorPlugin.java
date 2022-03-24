7f0031f240c5225fec534ebeaba21d1fc82de533
Corrado Lombardi <corrado.lombardi@4science.it>
2022-03-24 09:11:48 +0100

    [DSC-531] checkstyle fix

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/UsageReportRestPermissionEvaluatorPlugin.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/UsageReportRestPermissionEvaluatorPlugin.java
@@ -9,7 +9,6 @@ package org.dspace.app.rest.security;
 
 import java.io.Serializable;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
