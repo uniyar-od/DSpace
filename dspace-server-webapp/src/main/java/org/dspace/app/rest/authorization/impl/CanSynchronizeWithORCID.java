701cff289bd719e4bb93e3d67da70d33029275c2
Luca Giamminonni <luca.giamminonni@4science.it>
2022-07-22 17:13:42 +0200

    [CST-5515] Removed orcid boxes from CrisLayoutBoxServiceImpl.hasContent

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/authorization/impl/CanSynchronizeWithORCID.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/authorization/impl/CanSynchronizeWithORCID.java
@@ -8,21 +8,18 @@
 package org.dspace.app.rest.authorization.impl;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Predicate;
 
-import org.apache.commons.lang3.StringUtils;
 import org.dspace.app.rest.authorization.AuthorizationFeature;
 import org.dspace.app.rest.authorization.AuthorizationFeatureDocumentation;
 import org.dspace.app.rest.model.BaseObjectRest;
 import org.dspace.app.rest.model.ItemRest;
 import org.dspace.content.Item;
-import org.dspace.content.MetadataValue;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
+import org.dspace.eperson.service.EPersonService;
 import org.dspace.services.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -39,6 +36,9 @@ import org.springframework.stereotype.Component;
 public class CanSynchronizeWithORCID implements AuthorizationFeature {
 
     public static final String NAME = "canSynchronizeWithORCID";
+
+    @Autowired
+    private EPersonService ePersonService;
 
     @Autowired
     private ItemService itemService;
@@ -63,7 +63,7 @@ public class CanSynchronizeWithORCID implements AuthorizationFeature {
         String id = ((ItemRest) object).getId();
         Item item = itemService.find(context, UUID.fromString(id));
 
-        return isOrcidSynchronizationEnabled() && isDspaceObjectOwner(ePerson, item);
+        return isOrcidSynchronizationEnabled() && ePersonService.isOwnerOfItem(ePerson, item);
     }
 
     @Override
@@ -73,19 +73,5 @@ public class CanSynchronizeWithORCID implements AuthorizationFeature {
 
     private boolean isOrcidSynchronizationEnabled() {
         return configurationService.getBooleanProperty("orcid.synchronization-enabled", true);
-    }
-
-    /**
-     * This method returns true if the given eperson is not null and if the given
-     * item has the metadata field dspace.object.owner with an authority equals to
-     * the uuid of the eperson. Otherwise this method returns false.
-     */
-    private boolean isDspaceObjectOwner(EPerson eperson, Item item) {
-        if (eperson == null) {
-            return false;
-        }
-        List<MetadataValue> owners = itemService.getMetadataByMetadataString(item, "dspace.object.owner");
-        Predicate<MetadataValue> checkOwner = v -> StringUtils.equals(v.getAuthority(), eperson.getID().toString());
-        return owners.stream().anyMatch(checkOwner);
     }
 }
