22374fc72b2f561add9a4113acd56bfe5d216806
corrado lombardi <corrado.lombardi@4science.com>
2022-07-26 16:04:54 +0200

    [DSC-522] filter to use only Item instances in choices

--- dspace-api/src/main/java/org/dspace/content/authority/ItemControlledVocabularyService.java
+++ dspace-api/src/main/java/org/dspace/content/authority/ItemControlledVocabularyService.java
@@ -177,6 +177,7 @@ public class ItemControlledVocabularyService extends SelfNamedPlugin implements 
     private List<Choice> getChoicesFromResult(String authorityName, ItemControlledVocabulary controlledVocabulary,
                                               DiscoverResult result) {
         return result.getIndexableObjects().stream()
+            .filter(i -> i instanceof Item)
             .map(i -> {
                 Item item = (Item) i.getIndexedObject();
                 return getChoiceFromItem(authorityName, controlledVocabulary, item);
