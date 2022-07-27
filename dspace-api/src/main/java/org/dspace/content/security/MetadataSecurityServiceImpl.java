a57f733441293141e5225b1d995df35f515aa9a8
eskander <mohamed.eskander@atis.al>
2022-07-27 15:59:39 +0200

    [CST-6398] handle checkstyle

--- dspace-api/src/main/java/org/dspace/content/security/MetadataSecurityServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/security/MetadataSecurityServiceImpl.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
