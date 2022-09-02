44b49d42d8d25f1e674569e0a543569f7180d351
Stefano Maffei <stefano.maffei@4science.it>
2022-09-02 11:27:33 +0200

    [D4CRIS-1179] Fix SubmissionController error when symbolic path is used for the upload dir

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/SubmissionController.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/SubmissionController.java
@@ -1732,7 +1732,7 @@ public class SubmissionController extends DSpaceServlet
         File fileDir = new File(tempDir);
 
         // Test fileDir to see if canonical path is within the original baseDir
-        if(!fileDir.getCanonicalPath().startsWith(baseDir)) {
+        if(!fileDir.getAbsolutePath().startsWith(baseDir)) {
             log.error("Error processing resumable upload chunk: temporary chunk file would be created outside " +
                     "permissible temp dir ("+ baseDir +") for submitter: " + context.getCurrentUser().getEmail());
             throw new IOException("Error processing resumableIdentifier: " + resumableIdentifier +
@@ -1750,7 +1750,7 @@ public class SubmissionController extends DSpaceServlet
         File chunkFile = new File(chunkPath);
 
         // Test chunkFile to see if canonical path is within the original baseDir
-        if(!chunkFile.getCanonicalPath().startsWith(baseDir)) {
+        if(!chunkFile.getAbsolutePath().startsWith(baseDir)) {
             log.error("Error processing resumable upload chunk: temporary chunk file would be created outside " +
                     "permissible temp dir ("+ baseDir +") for submitter: " + context.getCurrentUser().getEmail());
             throw new IOException("Error processing resumableIdentifier: " + resumableIdentifier +
