9116142ee210a88f08e881cedce62e91dd44788f
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2022-09-22 18:34:13 +0200

    [CST-5699] fix SAF import and export functionality; add ITs for SAF via CLI

--- dspace-api/src/main/java/org/dspace/app/itemimport/ItemImportServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/app/itemimport/ItemImportServiceImpl.java
@@ -2055,7 +2055,6 @@ public class ItemImportServiceImpl implements ItemImportService, InitializingBea
         final String theFilePath = filepath;
         final String theInputType = inputType;
         final String theResumeDir = resumeDir;
-        final boolean useTemplateItem = template;
 
         Thread go = new Thread() {
             @Override
