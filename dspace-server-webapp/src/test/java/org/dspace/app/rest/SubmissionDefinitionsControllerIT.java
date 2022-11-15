2a85b28aeed9e07bb13c17f18100166d3e8a8ad1
Mykhaylo <michele.boychuk@gmail.com>
2022-11-15 13:59:41 +0100

    fix failed tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/SubmissionDefinitionsControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/SubmissionDefinitionsControllerIT.java
@@ -310,10 +310,10 @@ public class SubmissionDefinitionsControllerIT extends AbstractControllerIntegra
                         Matchers.containsString("page=1"), Matchers.containsString("size=1"))))
                 .andExpect(jsonPath("$._links.last.href", Matchers.allOf(
                         Matchers.containsString("/api/config/submissiondefinitions?"),
-                        Matchers.containsString("page=13"), Matchers.containsString("size=1"))))
+                        Matchers.containsString("page=14"), Matchers.containsString("size=1"))))
                 .andExpect(jsonPath("$.page.size", is(1)))
-                .andExpect(jsonPath("$.page.totalElements", is(14)))
-                .andExpect(jsonPath("$.page.totalPages", is(14)))
+                .andExpect(jsonPath("$.page.totalElements", is(15)))
+                .andExpect(jsonPath("$.page.totalPages", is(15)))
                 .andExpect(jsonPath("$.page.number", is(0)));
 
         getClient(tokenAdmin).perform(get("/api/config/submissiondefinitions")
@@ -336,10 +336,10 @@ public class SubmissionDefinitionsControllerIT extends AbstractControllerIntegra
                         Matchers.containsString("page=1"), Matchers.containsString("size=1"))))
                 .andExpect(jsonPath("$._links.last.href", Matchers.allOf(
                         Matchers.containsString("/api/config/submissiondefinitions?"),
-                        Matchers.containsString("page=13"), Matchers.containsString("size=1"))))
+                        Matchers.containsString("page=14"), Matchers.containsString("size=1"))))
                 .andExpect(jsonPath("$.page.size", is(1)))
-                .andExpect(jsonPath("$.page.totalElements", is(14)))
-                .andExpect(jsonPath("$.page.totalPages", is(14)))
+                .andExpect(jsonPath("$.page.totalElements", is(15)))
+                .andExpect(jsonPath("$.page.totalPages", is(15)))
                 .andExpect(jsonPath("$.page.number", is(1)));
     }
 }
