8f6ce0cae0b71f493cab3b25a7bf50e54ff33186
Luca Giamminonni <luca.giamminonni@4science.it>
2022-12-21 15:28:35 +0100

    [DSC-793] Fixed checkstyle warnings

--- dspace-api/src/main/java/org/dspace/eperson/AccountServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/eperson/AccountServiceImpl.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 import javax.mail.MessagingException;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dspace.authorize.AuthorizeException;
