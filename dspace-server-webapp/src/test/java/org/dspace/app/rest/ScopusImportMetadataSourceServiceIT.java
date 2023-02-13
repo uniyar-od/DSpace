80f8f1379a64a5dd1398cbe7af9949cc2c70bc2f
eskander <mohamed.eskander@atis.al>
2023-02-13 15:31:31 +0200

    [DSC-925] solved Missing authors mapping

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ScopusImportMetadataSourceServiceIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ScopusImportMetadataSourceServiceIT.java
@@ -25,7 +25,6 @@ import org.dspace.importer.external.datamodel.ImportRecord;
 import org.dspace.importer.external.liveimportclient.service.LiveImportClientImpl;
 import org.dspace.importer.external.metadatamapping.MetadatumDTO;
 import org.dspace.importer.external.scopus.service.ScopusImportMetadataSourceServiceImpl;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
@@ -45,7 +44,6 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
     private LiveImportClientImpl liveImportClientImpl;
 
     @Test
-    @Ignore
     public void scopusImportMetadataGetRecordsTest() throws Exception {
         context.turnOffAuthorisationSystem();
         String originApiKey = scopusServiceImpl.getApiKey();
@@ -98,7 +96,7 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
     }
 
     @Test
-    public void scopusImportMetadataGetRecordsEmptyResponceTest() throws Exception {
+    public void scopusImportMetadataGetRecordsEmptyResponseTest() throws Exception {
         context.turnOffAuthorisationSystem();
         String originApiKey = scopusServiceImpl.getApiKey();
         if (StringUtils.isBlank(originApiKey)) {
@@ -123,6 +121,62 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         }
     }
 
+    @Test
+    public void scopusImportMetadataGetRecordTest() throws Exception {
+        context.turnOffAuthorisationSystem();
+        String originApiKey = scopusServiceImpl.getApiKey();
+        if (StringUtils.isBlank(originApiKey)) {
+            scopusServiceImpl.setApiKey("testApiKey");
+        }
+        CloseableHttpClient originalHttpClient = liveImportClientImpl.getHttpClient();
+        CloseableHttpClient httpClient = Mockito.mock(CloseableHttpClient.class);
+        try (InputStream file = getClass().getResourceAsStream("scopus-ex.xml")) {
+            String scopusXmlResp = IOUtils.toString(file, Charset.defaultCharset());
+
+            liveImportClientImpl.setHttpClient(httpClient);
+            CloseableHttpResponse response = mockResponse(scopusXmlResp, 200, "OK");
+            when(httpClient.execute(ArgumentMatchers.any())).thenReturn(response);
+
+            context.restoreAuthSystemState();
+
+            ImportRecord importRecord = scopusServiceImpl.getRecord("2-s2.0-85124241875");
+            matchRecord(importRecord, getRecords().get(0));
+        } finally {
+            liveImportClientImpl.setHttpClient(originalHttpClient);
+            scopusServiceImpl.setApiKey(originApiKey);
+        }
+    }
+
+    @Test
+    public void scopusImportAuthorMetadataWhenAuthorsElementsMissedTest() throws Exception {
+        context.turnOffAuthorisationSystem();
+        String originApiKey = scopusServiceImpl.getApiKey();
+        if (StringUtils.isBlank(originApiKey)) {
+            scopusServiceImpl.setApiKey("testApiKey");
+        }
+        CloseableHttpClient originalHttpClient = liveImportClientImpl.getHttpClient();
+        CloseableHttpClient httpClient = Mockito.mock(CloseableHttpClient.class);
+        try {
+            String scopusXmlResp = getResponseWithCreatorAndWithoutAuthorsElement();
+
+            liveImportClientImpl.setHttpClient(httpClient);
+            CloseableHttpResponse response = mockResponse(scopusXmlResp, 200, "OK");
+            when(httpClient.execute(ArgumentMatchers.any())).thenReturn(response);
+
+            context.restoreAuthSystemState();
+
+            MetadatumDTO createdMetadata = createMetadatumDTO("dc", "contributor", "author", "Sforza L.");
+            ImportRecord importRecord = scopusServiceImpl.getRecord("2-s2.0-85128066737");
+
+            assertEquals(importRecord.getValue("dc", "contributor", "author").size(), 1);
+            matchMetadataValue(new ArrayList<>(importRecord.getValue("dc", "contributor", "author")),
+                List.of(createdMetadata));
+        } finally {
+            liveImportClientImpl.setHttpClient(originalHttpClient);
+            scopusServiceImpl.setApiKey(originApiKey);
+        }
+    }
+
     private ArrayList<ImportRecord> getRecords() {
         ArrayList<ImportRecord> records = new ArrayList<>();
         //define first record
@@ -132,18 +186,18 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
                 "Hardy potential versus lower order terms in Dirichlet problems: regularizing effects<sup>†</sup>");
         MetadatumDTO type = createMetadatumDTO("dc", "type", null, "Journal");
         MetadatumDTO date = createMetadatumDTO("dc", "date", "issued", "2023-01-01");
-        MetadatumDTO scopusId = createMetadatumDTO("dc", "identifier", "other", "2-s2.0-85124241875");
+        MetadatumDTO scopusId = createMetadatumDTO("dc", "identifier", "scopus", "2-s2.0-85124241875");
         MetadatumDTO citationVolume = createMetadatumDTO("oaire", "citation", "volume", "5");
         MetadatumDTO citationIssue = createMetadatumDTO("oaire", "citation", "issue", "1");
         MetadatumDTO subject = createMetadatumDTO("dc", "subject", null,
                                                   "Hardy potentials | Laplace equation | Summability of solutions");
-        MetadatumDTO author = createMetadatumDTO("dc", "contributor", "author", "Arcoya D.");
+        MetadatumDTO author = createMetadatumDTO("dc", "contributor", "author", "Arcoya, David");
         MetadatumDTO scopusAuthorId = createMetadatumDTO("person", "identifier", "scopus-author-id", "6602330574");
         MetadatumDTO orgunit = createMetadatumDTO("person", "affiliation", "name", "Universidad de Granada");
-        MetadatumDTO author2 = createMetadatumDTO("dc", "contributor", "author", "Boccardo L.");
+        MetadatumDTO author2 = createMetadatumDTO("dc", "contributor", "author", "Boccardo, Lucio");
         MetadatumDTO scopusAuthorId2 = createMetadatumDTO("person", "identifier", "scopus-author-id", "7003612261");
         MetadatumDTO orgunit2 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
-        MetadatumDTO author3 = createMetadatumDTO("dc", "contributor", "author", "Orsina L.");
+        MetadatumDTO author3 = createMetadatumDTO("dc", "contributor", "author", "Orsina, Luigi");
         MetadatumDTO scopusAuthorId3 = createMetadatumDTO("person", "identifier", "scopus-author-id", "6602595438");
         MetadatumDTO orgunit3 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
         MetadatumDTO rights = createMetadatumDTO("dc", "rights", null, "true");
@@ -158,6 +212,9 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         metadatums.add(citationVolume);
         metadatums.add(citationIssue);
         metadatums.add(subject);
+        metadatums.add(rights);
+        metadatums.add(ispartof);
+        metadatums.add(ispartofseries);
         metadatums.add(author);
         metadatums.add(scopusAuthorId);
         metadatums.add(orgunit);
@@ -167,9 +224,6 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         metadatums.add(author3);
         metadatums.add(scopusAuthorId3);
         metadatums.add(orgunit3);
-        metadatums.add(rights);
-        metadatums.add(ispartof);
-        metadatums.add(ispartofseries);
         ImportRecord firstrRecord = new ImportRecord(metadatums);
 
         //define second record
@@ -178,7 +232,7 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         MetadatumDTO title2 = createMetadatumDTO("dc","title", null,
                 "Large deviations for a binary collision model: energy evaporation<sup>†</sup>");
         MetadatumDTO date2 = createMetadatumDTO("dc", "date", "issued", "2023-01-01");
-        MetadatumDTO scopusId2 = createMetadatumDTO("dc", "identifier", "other", "2-s2.0-85124226483");
+        MetadatumDTO scopusId2 = createMetadatumDTO("dc", "identifier", "scopus", "2-s2.0-85124226483");
         MetadatumDTO type2 = createMetadatumDTO("dc", "type", null, "Journal");
         MetadatumDTO citationVolume2 = createMetadatumDTO("oaire", "citation", "volume", "5");
         MetadatumDTO citationIssue2 = createMetadatumDTO("oaire", "citation", "issue", "1");
@@ -186,16 +240,16 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         MetadatumDTO subject2 = createMetadatumDTO("dc", "subject", null,
         "Boltzmann equation | Discrete energy model | Kac model | Large deviations | Violation of energy conservation");
 
-        MetadatumDTO author4 = createMetadatumDTO("dc", "contributor", "author", "Basile G.");
+        MetadatumDTO author4 = createMetadatumDTO("dc", "contributor", "author", "Basile, Giada");
         MetadatumDTO scopusAuthorId4 = createMetadatumDTO("person", "identifier", "scopus-author-id", "55613229065");
         MetadatumDTO orgunit4 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
-        MetadatumDTO author5 = createMetadatumDTO("dc", "contributor", "author", "Benedetto D.");
+        MetadatumDTO author5 = createMetadatumDTO("dc", "contributor", "author", "Benedetto, Dario");
         MetadatumDTO scopusAuthorId5 = createMetadatumDTO("person", "identifier", "scopus-author-id", "55893665100");
         MetadatumDTO orgunit5 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
-        MetadatumDTO author6 = createMetadatumDTO("dc", "contributor", "author", "Caglioti E.");
+        MetadatumDTO author6 = createMetadatumDTO("dc", "contributor", "author", "Caglioti, Emanuele");
         MetadatumDTO scopusAuthorId6 = createMetadatumDTO("person", "identifier", "scopus-author-id", "7004588675");
         MetadatumDTO orgunit6 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
-        MetadatumDTO author7 = createMetadatumDTO("dc", "contributor", "author", "Bertini L.");
+        MetadatumDTO author7 = createMetadatumDTO("dc", "contributor", "author", "Bertini, Lorenzo");
         MetadatumDTO scopusAuthorId7 = createMetadatumDTO("person", "identifier", "scopus-author-id", "7005555198");
         MetadatumDTO orgunit7 = createMetadatumDTO("person", "affiliation", "name","Sapienza Università di Roma");
         MetadatumDTO rights2 = createMetadatumDTO("dc", "rights", null, "true");
@@ -210,6 +264,9 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         metadatums2.add(citationVolume2);
         metadatums2.add(citationIssue2);
         metadatums2.add(subject2);
+        metadatums2.add(rights2);
+        metadatums2.add(ispartof2);
+        metadatums2.add(ispartofseries2);
         metadatums2.add(author4);
         metadatums2.add(scopusAuthorId4);
         metadatums2.add(orgunit4);
@@ -222,13 +279,32 @@ public class ScopusImportMetadataSourceServiceIT extends AbstractLiveImportInteg
         metadatums2.add(author7);
         metadatums2.add(scopusAuthorId7);
         metadatums2.add(orgunit7);
-        metadatums2.add(rights2);
-        metadatums2.add(ispartof2);
-        metadatums2.add(ispartofseries2);
         ImportRecord secondRecord = new ImportRecord(metadatums2);
         records.add(firstrRecord);
         records.add(secondRecord);
         return records;
+    }
+
+    private String getResponseWithCreatorAndWithoutAuthorsElement() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<search-results " +
+            "xmlns=\"http://www.w3.org/2005/Atom\" " +
+            "xmlns:dc=\"http://purl.org/dc/elements/1.1/\" " +
+            "xmlns:opensearch=\"http://a9.com/-/spec/opensearch/1.1/\" " +
+            "xmlns:prism=\"http://prismstandard.org/namespaces/basic/2.0/\" " +
+            "xmlns:atom=\"http://www.w3.org/2005/Atom\">\n" +
+            "    <opensearch:totalResults>1</opensearch:totalResults>\n" +
+            "    <opensearch:startIndex>0</opensearch:startIndex>\n" +
+            "    <opensearch:itemsPerPage>1</opensearch:itemsPerPage>\n" +
+            "    <opensearch:Query role=\"request\" searchTerms=\"EID(2-s2.0-85128066737)\" startPage=\"0\"/>\n" +
+            "    <link ref=\"self\" href=\"https://api.elsevier.com/content/search/scopus?start=0&amp;count=25&amp;" +
+            "query=EID%282-s2.0-85128066737%29\" type=\"application/xml\"/>\n" +
+            "    <link ref=\"first\" href=\"https://api.elsevier.com/content/search/scopus?start=0&amp;count=25&amp;" +
+            "query=EID%282-s2.0-85128066737%29\" type=\"application/xml\"/>\n" +
+            "<entry>\n" +
+            "<dc:creator>Sforza L.</dc:creator>\n" +
+            "</entry>\n" +
+            "</search-results>";
     }
 
 }
